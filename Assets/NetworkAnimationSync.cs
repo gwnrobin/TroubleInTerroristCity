83b155f0d714466937b847f5001031f4d685d9a2
gwnrobin <robinschellingerhout@live.nl>
2023-10-17 15:29:35 +0200

    Cleanup

--- Assets/NetworkAnimationSync.cs
+++
@@ -1,8 +0,0 @@
-using Kinemation.FPSFramework.Runtime.Core.Components;
-using Kinemation.FPSFramework.Runtime.Core.Types;
-using Kinemation.FPSFramework.Runtime.Recoil;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Netcode;
-using UnityEngine;
-
