41a8ae7648397a0cf1693f760bd1337f05df232d
gwnrobin <robinschellingerhout@live.nl>
2024-01-07 21:20:27 +0100

    more baked lights

--- Assets/_Code/UI_PlayerInteraction.cs
+++ Assets/_Code/UI_PlayerInteraction.cs
@@ -1,62 +1,57 @@
-using AYellowpaper.SerializedCollections.Editor.Data;
 using UnityEngine;
-using UnityEngine.UI;
 
-namespace HQFPSTemplate.UserInterface
+public class UI_PlayerInteraction : MonoBehaviour
 {
-	public class UI_PlayerInteraction : MonoBehaviour
-	{
-		#region Anim Hashing
-		//Hashed animator strings (Improves performance)
-		private readonly int animHash_Show = Animator.StringToHash("Show");
-        #endregion
+    #region Anim Hashing
+    //Hashed animator strings (Improves performance)
+    private readonly int animHash_Show = Animator.StringToHash("Show");
+    #endregion
 
-        [BHeader("Generic Interaction", true)]
+    [BHeader("Generic Interaction", true)]
 
-		[SerializeField]
-		private ChangeText m_GenericText = null;
+    [SerializeField]
+    private ChangeText m_GenericText = null;
 
-		[BHeader("Equipment Specific Interaction", true)]
+    [BHeader("Equipment Specific Interaction", true)]
 		
-		private RaycastInfo m_RaycastData;
-		private bool m_SwapUIEnabled;
+    private RaycastInfo m_RaycastData;
+    private bool m_SwapUIEnabled;
 		
-		public void BindPlayer(Player player)
-		{
-			if (player != null)
-			{
-				player.RaycastInfo.AddChangeListener(OnPlayerRaycastChanged);
-				player.EquippedItem.AddChangeListener((Item item) =>
-				{
-					OnPlayerRaycastChanged(player.RaycastInfo.Val);
-				});
-			}
-		}
+    public void BindPlayer(Player player)
+    {
+        if (player != null)
+        {
+            player.RaycastInfo.AddChangeListener(OnPlayerRaycastChanged);
+            player.EquippedItem.AddChangeListener((Item item) =>
+            {
+                OnPlayerRaycastChanged(player.RaycastInfo.Val);
+            });
+        }
+    }
 
-		private void OnPlayerRaycastChanged(RaycastInfo raycastData)
-		{
-			bool show = raycastData != null && raycastData.IsInteractive;
+    private void OnPlayerRaycastChanged(RaycastInfo raycastData)
+    {
+        bool show = raycastData != null && raycastData.IsInteractive;
 			
-			if(m_RaycastData != null)
-				m_RaycastData.InteractiveObject.InteractionText.RemoveChangeListener(UpdateInteractText);
+        if(m_RaycastData != null)
+            m_RaycastData.InteractiveObject.InteractionText.RemoveChangeListener(UpdateInteractText);
 
-			m_RaycastData = raycastData;
+        m_RaycastData = raycastData;
 
-			if (show)
-			{
-				UpdateInteractText(m_RaycastData.InteractiveObject.InteractionText.Val);
+        if (show)
+        {
+            UpdateInteractText(m_RaycastData.InteractiveObject.InteractionText.Val);
 				
-				m_RaycastData.InteractiveObject.InteractionText.AddChangeListener(UpdateInteractText);
-			}
-			else
-			{
-				UpdateInteractText("");
-			}
-		}
+            m_RaycastData.InteractiveObject.InteractionText.AddChangeListener(UpdateInteractText);
+        }
+        else
+        {
+            UpdateInteractText("");
+        }
+    }
 
-		private void UpdateInteractText(string text)
-		{
-			m_GenericText.ChangeTextTo(text);
-		}
-	}
+    private void UpdateInteractText(string text)
+    {
+        m_GenericText.ChangeTextTo(text);
+    }
 }
